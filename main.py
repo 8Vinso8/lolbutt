65264a69dd3e1740c18fba098adfb410ac06b28c
Vinso <shamilka2003@gmail.com>
2020-04-13 21:41:17 +1000

    Вход готов

--- main.py
+++ main.py
@@ -4,12 +4,21 @@ from register import RegistrationForm
 from data import db_session
 from data.users import User
 import cassiopeia as cass
+from flask_login import LoginManager, login_user
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
 cass.set_riot_api_key("RGAPI-d8b0420d-7d54-4e07-9128-1fcfdb74bf51")
 cass.set_default_region("RU")
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    session = db_session.create_session()
+    return session.query(User).get(user_id)
 
 
 @app.route('/')
@@ -26,8 +35,15 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect('/')
-    return render_template('login.html', form=form)
+        session = db_session.create_session()
+        user = session.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
