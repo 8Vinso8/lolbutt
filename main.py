be705394d40592ff552d128f8f7fc8e44dc1fdf6
Ultra <agent002004@gmail.com>
2020-04-13 22:14:26 +1000

    кнопка поиска работает

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from data import db_session
 from data.users import User
 import cassiopeia as cass
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 cass.set_riot_api_key("RGAPI-d8b0420d-7d54-4e07-9128-1fcfdb74bf51")
@@ -57,18 +56,16 @@ def register():
     return render_template('register.html', form=form)
 
 
-@app.route('/search/<summoner_name>')
-def search(summoner_name):
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    summoner_name = request.form.get('summoner_name')
     summoner = cass.get_summoner(name=summoner_name)
-    print("{name} is a level {level} summoner on the {region} server.".format(name=summoner.name,
-                                                                              level=summoner.level,
-                                                                              region=summoner.region))
-
-
-def main():
-    db_session.global_init("db/data.sqlite")
-    app.run(host='127.0.0.1', port='8080', debug=True)
+    name = summoner.name
+    level = summoner.level
+    region = summoner.region
+    return render_template('search.html', name=name, level=level, region=region)
 
 
 if __name__ == '__main__':
-    main()
+    db_session.global_init("db/data.sqlite")
+    app.run(host='127.0.0.1', port='8080', debug=True)
