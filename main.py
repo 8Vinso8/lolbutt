e3543721e85722608ae38844911019842d960447
Ultra <agent002004@gmail.com>
2020-04-13 21:19:36 +1000

    кнопки регистрации и логина работают; поиск в процессе

--- main.py
+++ main.py
@@ -2,15 +2,23 @@ from flask import Flask, request, render_template, redirect
 from login import LoginForm
 from register import RegistrationForm
 from data import db_session
+import cassiopeia as cass
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+cass.set_riot_api_key("RGAPI-d8b0420d-7d54-4e07-9128-1fcfdb74bf51")
+cass.set_default_region("RU")
 
 
 @app.route('/')
+def base():
+    return render_template('base.html')
+
+
 @app.route('/index')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -34,6 +42,14 @@ def register():
         print(the_email, the_username, the_password)
         return redirect('/')
     return render_template('register.html', form=form)
+
+
+@app.route('/search/<summoner_name>')
+def search(summoner_name):
+    summoner = cass.get_summoner(name=summoner_name)
+    print("{name} is a level {level} summoner on the {region} server.".format(name=summoner.name,
+                                                                              level=summoner.level,
+                                                                              region=summoner.region))
 
 
 def main():
