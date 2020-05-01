c63ba6f90e77f37a1e77bbc884a55bf420849ed1
Vring <44764023+Vring@users.noreply.github.com>
2020-05-01 23:13:15 +1000

    Update main.py

--- main.py
+++ main.py
@@ -202,4 +202,4 @@ def activate(token):
 if __name__ == '__main__':
     db_session.global_init("db/data.sqlite")
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='localhost', port=port)
+    app.run(host='0.0.0.0', port=port)
