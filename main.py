dc8b027b931988443930e2a36e93c2bd481f9f10
Vring <44764023+Vring@users.noreply.github.com>
2020-05-01 22:41:27 +1000

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
 from uuid import uuid4
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -200,4 +201,6 @@ def activate(token):
 
 if __name__ == '__main__':
     db_session.global_init("db/data.sqlite")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     app.run(host='0.0.0.0', port=5000)
